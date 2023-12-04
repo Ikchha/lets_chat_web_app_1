9ea928a192cfff6c029537d5210d4728cc16e16a
Ikchha <102365567+Ikchha@users.noreply.github.com>
2023-12-04 20:50:52 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,13 +1,17 @@
 var firebaseConfig = {
   apiKey: "AIzaSyDBdGnABIz7I_via8AvodSfXmYW2NS0OKM",
   authDomain: "lets-chat-web-app-764e5.firebaseapp.com",
-  databaseURL: "https://lets-chat-web-app-764e5-default-rtdb.firebaseio.com",
+  databaseURL: "https://lets-chat-web-app-764e5-default-rtdb.firebaseio.com/",
   projectId: "lets-chat-web-app-764e5",
   storageBucket: "lets-chat-web-app-764e5.appspot.com",
   messagingSenderId: "570173575054",
   appId: "1:570173575054:web:0b54a29ab0cb0a5de35edf"
   };
   
-   firebaseConfig.initializeApp(firebaseConfig);
+   firebase.initializeApp(firebaseConfig);
+
+   user_name = localStorage.getItem("user_name");
+   document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!";
+
 
   
